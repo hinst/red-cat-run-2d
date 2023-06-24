b753280d0858e79f34eb0ca02a86aba33346fe5c
hinst <hinstance@yandex.ru>
2023-06-24 13:48:08 +0200

    Updated shaft color: blue -> brown

--- gameSceneElements.go
+++ gameSceneElements.go
@@ -7,8 +7,12 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
-var SHAFT_COLOR_MULTIPLIER = float64(100) / 255
-var SHAFT_COLOR = color.RGBA{R: 0, G: uint8(43 * SHAFT_COLOR_MULTIPLIER), B: uint8(54 * SHAFT_COLOR_MULTIPLIER), A: 255}
+var SHAFT_COLOR_MULTIPLIER = float64(127) / 255
+var SHAFT_COLOR = color.RGBA{
+	R: uint8(50 * SHAFT_COLOR_MULTIPLIER),
+	G: uint8(35 * SHAFT_COLOR_MULTIPLIER),
+	B: uint8(27 * SHAFT_COLOR_MULTIPLIER),
+	A: 255}
 var TORCH_LIGHT_COLOR = color.NRGBA{R: 255, G: 244, B: 188, A: 10}
 
 func DrawTorchLight(screen *ebiten.Image, x float32, y float32) {
