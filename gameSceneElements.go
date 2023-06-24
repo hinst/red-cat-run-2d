1b9cfbb149ff78704c8599b644f78722da4fa378
hinst <hinstance@yandex.ru>
2023-06-24 13:26:40 +0200

    Making shaft non-transparent

--- gameSceneElements.go
+++ gameSceneElements.go
@@ -7,8 +7,10 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
-var SHAFT_COLOR = color.NRGBA{R: 0, G: 43, B: 54, A: 64}
+var SHAFT_COLOR_MULTIPLIER = float64(100) / 255
+var SHAFT_COLOR = color.RGBA{R: 0, G: uint8(43 * SHAFT_COLOR_MULTIPLIER), B: uint8(54 * SHAFT_COLOR_MULTIPLIER), A: 255}
+var TORCH_LIGHT_COLOR = color.NRGBA{R: 255, G: 244, B: 188, A: 10}
 
 func DrawTorchLight(screen *ebiten.Image, x float32, y float32) {
-	vector.DrawFilledCircle(screen, x, y, 16, color.NRGBA{R: 255, G: 244, B: 188, A: 20}, false)
+	vector.DrawFilledCircle(screen, x, y, 16, TORCH_LIGHT_COLOR, false)
 }
