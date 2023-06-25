9c852d12ed11c6fece5472e09b8066659b72da1e
hinst <hinstance@yandex.ru>
2023-06-25 16:41:45 +0200

    Create zip archive for web

--- build-web.bat
+++ build-web.bat
@@ -4,4 +4,4 @@ go build -o red-cat-run-2d.wasm .
 copy "C:\Program Files\Go\misc\wasm\wasm_exec.js" red-cat-run-2d.js
 set GOOS=
 set GOARCH=
-rem Create zip archive from files index.html red-cat-run-2d.js red-cat-run-2d.wasm
+7z a -tzip red-cat-run-2d.zip index.html red-cat-run-2d.js red-cat-run-2d.wasm
