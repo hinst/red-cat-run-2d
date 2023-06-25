b2ceda9e454c67167370d74430bf0bac8635f89a
hinst <hinstance@yandex.ru>
2023-06-25 19:22:27 +0200

    Slim web build

--- build-web.bat
+++ build-web.bat
@@ -1,6 +1,6 @@
 set GOOS=js
 set GOARCH=wasm
-go build -o red-cat-run-2d.wasm .
+go build -o red-cat-run-2d.wasm -ldflags="-s -w" .
 copy "C:\Program Files\Go\misc\wasm\wasm_exec.js" red-cat-run-2d.js
 set GOOS=
 set GOARCH=
