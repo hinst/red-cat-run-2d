d1574ae475d1b3dff0625ce57a99ebe89b55b9a2
hinst <hinstance@yandex.ru>
2023-06-25 14:48:23 +0200

    Enable vsync

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	debug.SetGCPercent(50)
 	ebiten.SetWindowSize(840, 480)
 	ebiten.SetWindowTitle("RED CAT RUN 2D")
-	ebiten.SetVsyncEnabled(false)
+	ebiten.SetVsyncEnabled(true)
 	var game = &Game{}
 	game.Initialize()
 	if err := ebiten.RunGame(game); err != nil {
