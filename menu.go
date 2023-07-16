8f38a2410bf6bea458cfea366b20f87dd01cb384
hinst <hinstance@yandex.ru>
2023-07-16 12:48:04 +0200

    Set alpha channel for menu selection rectangle

--- menu.go
+++ menu.go
@@ -69,7 +69,7 @@ func (me *MenuUserInterface) Draw(screen *ebiten.Image) {
 			2+float32(me.GetCharacterWidth()*len(me.Items[me.SelectedItemIndex].Title))+2,
 			float32(me.GetCharacterHeight()),
 			2,
-			color.RGBA{R: 0, G: uint8(math.Round(200 * math.Sin(me.PulseAnimationCounter))), B: 0, A: 0},
+			color.RGBA{R: 0, G: uint8(math.Round(200 * math.Sin(me.PulseAnimationCounter))), B: 0, A: 200},
 			false,
 		)
 	}
